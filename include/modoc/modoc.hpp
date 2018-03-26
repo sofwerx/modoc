050496b9b1e28843aac058d000ce0c65dd10b8d0
Gary Hendrick <gary.hendrick@gmail.com>
2018-03-26 16:37:57 -0400

    added the modoc header

---
+++ include/modoc/modoc.hpp
@@ -0,0 +1,43 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  modoc.hpp
+ *
+ *    Description:  MODOC is the mavros object designed only for communication
+ *
+ *        Version:  1.0
+ *        Created:  03/26/2018 04:33:28 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *        License:  MIT
+ *         Author:  Gary Hendrick gary.hendrick.fellow@sofwerx.org
+ *   Organization:  SOFWerx
+ *
+ * =====================================================================================
+ *  Copyright 2018 Gary Hendrick 
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of
+ *  this software and associated documentation files (the "Software"), to deal in
+ *  the Software without restriction, including without limitation the rights to
+ *  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ *  of the Software, and to permit persons to whom the Software is furnished to do
+ *  so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.*
+ */
+
+#ifndef __MODOC_H_INCLUDED__
+#define __MODOC_H_INCLUDED__
+
+#include <ros/ros.h>
+
+#endif
